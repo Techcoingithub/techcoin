b0ffc98420bc47ca737e0e55892bb05259105a87
Andrew J Hacker <andrew@thought.live>
2021-09-27 11:28:30 -0400

    add premine

--- src/validation.cpp
+++ src/validation.cpp
@@ -1176,9 +1176,18 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 
     CAmount nSubsidy = 50 * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-    nSubsidy >>= halvings;
-    return nSubsidy;
-}
+
+    if ((nHeight) == 1)
+      {
+          nSubsidy = 2950000 * COIN; // premine on block 1
+      }
+      else
+      {
+        nSubsidy >>= halvings;
+      }
+
+      return nSubsidy;
+  }
 
 CoinsViews::CoinsViews(
     std::string ldb_name,
